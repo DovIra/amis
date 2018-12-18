ff3b0df01449ba6e94b493291727920749e58f88
Iriska <iriska.dovgal@gmail.com>
2018-12-19 01:12:14 +0200

    plotly

--- km-83/Dovgal_Iryna/workshop6/homework/task1.py
+++ km-83/Dovgal_Iryna/workshop6/homework/task1.py
@@ -1,7 +1,27 @@
-"""
-Тут написати умову до завдання
-"""
+import re
+import plotly
+import plotly.graph_objs as go
+
+with open("/Users/iradovgal/PycharmProjects/data/data/mini_gun.csv", mode="r", encoding="utf-8")as file:
+    dataset = dict()
+    file.readline()
+
+    for line in file:
+        columns = line.split(",")
+
+        date = columns[1]
+        n_killed = columns[5]
+
+        if date not in dataset:
+            dataset[date] = list()
+
+        if n_killed not in dataset[date]:
+            dataset[date].append(n_killed)
 
 
 
-#TODO
+print(dataset)
+diagram = [go.Bar(x = list(dataset.keys()),
+                  y = list(dataset.values()))]
+plotly.offline.plot(diagram)
+
