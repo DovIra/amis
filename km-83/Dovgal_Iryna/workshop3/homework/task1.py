b82789b7d55227709d82a964633d4280556cd562
Iriska <iriska.dovgal@gmail.com>
2018-12-18 10:33:46 +0200

    recurs1

--- km-83/Dovgal_Iryna/workshop3/homework/task1.py
+++ km-83/Dovgal_Iryna/workshop3/homework/task1.py
@@ -1,7 +1,10 @@
-"""
-Тут написати умову до завдання
-"""
+def RecursionListSum(el):
+    if isinstance(el, list):
+        sum = 0
+        for item in el:
+            sum = sum + RecursionListSum(item)
 
-
-
-#TODO
+        return sum
+    else:
+        return el
+print(RecursionListSum([2,[2,[3],[2,2]],4]))
