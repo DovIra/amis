87c4b30781b7f2b718fa016f0e1b44052bf115b0
Iriska <iriska.dovgal@gmail.com>
2018-12-18 10:50:44 +0200

    task1

--- km-83/Dovgal_Iryna/workshop1/homework/task1.py
+++ km-83/Dovgal_Iryna/workshop1/homework/task1.py
@@ -5,4 +5,21 @@ def sum_n(n):
 
 print(sum_n(3))
 
+
+print("Довгаль Ірина\n КМ-83\nвариант 4")
+print(" Функція - Parse(s,t). "
+      "Призначення - поділ рядка s на дві частини: до першого входження символу t і після нього. \n\n")
+a =input("введіть рядок")
+b =input("введіть літеру")
+def Parse(s, t):
+    print(s[0:s.find(t)])
+    print(s[s.find(t):])
+    return "finish"
+
+if b in a:
+    Parse(a,b)
+else:
+    print("символу немає у рядку")
+
+
 ##
